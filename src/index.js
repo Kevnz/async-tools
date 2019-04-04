c03ac8bbabb2eebc6f263b04b5632c47b73c88a1
Kevin Isom <kevin@isom.nz>
2019-04-05 08:36:09 +1300

    export composer and holder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+const composer = require('./composer')
 const delay = require('./delay')
 const each = require('./each')
+const holder = require('./holder')
 const mapper = require('./mapper')
 const reducer = require('./reducer')
 
 module.exports = {
+  composer,
   delay,
   each,
+  holder,
   mapper,
   reducer,
 }
